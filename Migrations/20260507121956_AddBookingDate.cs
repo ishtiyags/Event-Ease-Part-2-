f01abd954533e063263312709e75aa39b020471f
ishtiyags <165825704+ishtiyags@users.noreply.github.com>
2026-05-07 20:26:03 +0200

    Add files via upload

---
+++ Migrations/20260507121956_AddBookingDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+
+
+#nullable disable
+
+namespace EventEase.Migrations
+{
+    public partial class AddBookingDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BookingDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookingDate",
+                table: "Bookings");
+        }
+    }
+}
